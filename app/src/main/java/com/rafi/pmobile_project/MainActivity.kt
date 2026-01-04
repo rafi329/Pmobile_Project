08cb2b3f089d092be56075762dc896648a3d0dfc
rafi329 <2300018329@webmail.uad.ac.id>
2026-01-04 21:09:53 +0700

    baru

--- app/src/main/java/com/rafi/pmobile_project/MainActivity.kt
+++ app/src/main/java/com/rafi/pmobile_project/MainActivity.kt
@@ -2,6 +2,9 @@ package com.rafi.pmobile_project
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -17,31 +20,73 @@ class MainActivity : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.rvProducts)
         val btnCart = findViewById<ImageView>(R.id.btnCart)
         val btnReceipt = findViewById<ImageView>(R.id.btnReceiptList)
-        val btnProfile = findViewById<ImageView>(R.id.btnProfile)
+        val editSearch = findViewById<EditText>(R.id.editSearch)
 
-        val produkList = listOf(
+        // DATA PRODUK
+        val allProducts = listOf(
             Product("Laptop Pro 15\"", "Laptop", 15999000, 15, R.drawable.laptop),
             Product("Wireless Headphone", "Audio", 2499000, 30, R.drawable.wireless_headphone),
             Product("Smartphone X", "Smartphone", 8999000, 20, R.drawable.smartphone),
             Product("Kamera Pro", "Kamera", 12499000, 10, R.drawable.kamera_pro)
         )
 
-        recyclerView.layoutManager = GridLayoutManager(this, 2)
-        recyclerView.adapter = ProductAdapter(produkList) { product ->
-            val existingItem = CartActivity.cartItems.find { it.product.nama == product.nama }
+        // DATA YANG DITAMPILKAN SAAT SEARCING
+        val filteredProducts = allProducts.toMutableList()
+
+        val adapter = ProductAdapter(filteredProducts) { product ->
+            val existingItem =
+                CartActivity.cartItems.find { it.product.nama == product.nama }
 
             if (existingItem != null) {
                 existingItem.quantity++
                 Toast.makeText(
                     this,
-                    "${product.nama} ditambahkan ke keranjang (Total: ${existingItem.quantity})",
+                    "${product.nama} ditambahkan (Total: ${existingItem.quantity})",
                     Toast.LENGTH_SHORT
                 ).show()
             } else {
                 CartActivity.cartItems.add(CartItem(product))
-                Toast.makeText(this, "${product.nama} ditambahkan ke keranjang", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this,
+                    "${product.nama} ditambahkan ke keranjang",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
+
+        recyclerView.layoutManager = GridLayoutManager(this, 2)
+        recyclerView.adapter = adapter
+
+        //FUNGSI SEARCH PRODUK
+        editSearch.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {}
+
+            override fun onTextChanged(
+                s: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+                val keyword = s.toString().lowercase()
+
+                if (keyword.isEmpty()) {
+                    adapter.updateData(allProducts)
+                } else {
+                    val hasilFilter = allProducts.filter {
+                        it.nama.lowercase().contains(keyword)
+                    }
+                    adapter.updateData(hasilFilter)
+                }
+            }
+        })
+
 
         btnCart.setOnClickListener {
             startActivity(Intent(this, CartActivity::class.java))
@@ -49,10 +94,6 @@ class MainActivity : AppCompatActivity() {
 
         btnReceipt.setOnClickListener {
             startActivity(Intent(this, TransactionHistoryActivity::class.java))
-        }
-
-        btnProfile.setOnClickListener {
-            startActivity(Intent(this, ProfileActivity::class.java))
         }
     }
 }
